8c396ac815631fe05b49008cdb210a03a07847f0
sushi.at <sushi@opensky.to>
2021-05-03 13:18:28 +0100

    Initial import of blazor wasm project with a few modifications towards the designs of the web team

---
+++ OpenSky.Website/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSky.Website
+{
+    using OpenSkyApi;
+
+    using Radzen;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            var httpClient = new HttpClient();
+            builder.Services.AddScoped(sp => httpClient);
+            builder.Services.AddSingleton<OpenSkyService>(new OpenSkyService("https://api.opensky.to", httpClient));
+
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
