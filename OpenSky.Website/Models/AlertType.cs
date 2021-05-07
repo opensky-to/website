6f214113e92bd809719b19e97eb49b6a7b308b42
sushi.at <sushi@opensky.to>
2021-05-07 01:43:44 +0100

    Added fist working version of user login/logout
    Replaced old error component with full alerting service
    Got wasm local storage working, to persists session ids over page refreshes and browser closes?
    NavMenu not refreshing solved by implementing event from UserSessionService

---
+++ OpenSky.Website/Models/AlertType.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AlertType.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.Website.Models
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Alert type enumeration.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 06/05/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public enum AlertType
+    {
+        /// <summary>
+        /// Success alert.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// Error alert.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Info alert.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Warning alert.
+        /// </summary>
+        Warning
+    }
+}
