84865e94e5bdebeff99b7840645767b6112e0e62
sushi.at <sushi@opensky.to>
2021-05-07 23:43:07 +0100

    Registration working
    Messing with recaptcha badge
    Cleaned up user session service
    Cleaned up static pages ready for content (demo content gone)

--- OpenSky.Website/Services/UserSessionService.cs
+++ OpenSky.Website/Services/UserSessionService.cs
@@ -1,47 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserSessionService.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace OpenSky.Website.Services
 {
+    using System;
+    using System.Threading.Tasks;
+
     using Blazored.LocalStorage;
 
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// User session service.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 07/05/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
     public class UserSessionService
     {
-        public bool IsUserLoggedIn { get; private set; }
-
-        public string OpenSkyApiToken { get; private set; }
-
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The WASM local storage service.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
         private readonly ILocalStorageService localStore;
 
-        public async Task Initialize()
-        {
-            var existingToken = await this.localStore.GetItemAsStringAsync("OpenSkyApiToken");
-            if (!string.IsNullOrEmpty(existingToken))
-            {
-                this.IsUserLoggedIn = true;
-                this.OpenSkyApiToken = existingToken;
-            }
-        }
-
-        public event Func<string, Task> NotifyUserChanged;
-
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserSessionService"/> class.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 07/05/2021.
+        /// </remarks>
+        /// <param name="localStore">
+        /// The WASM local storage service.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
         public UserSessionService(ILocalStorageService localStore)
         {
             this.localStore = localStore;
         }
 
-        public async Task LoggedIn(string token)
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Occurs when the currently logged in user changed.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public event Func<bool, Task> NotifyUserChanged;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets a value indicating whether there is a user currently logged in.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public bool IsUserLoggedIn { get; private set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the current OpenSky API token, null if no token is available.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public string OpenSkyApiToken { get; private set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the username (for display purposes only).
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public string Username { get; private set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Initialize the service.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 07/05/2021.
+        /// </remarks>
+        /// <returns>
+        /// An asynchronous result.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public async Task Initialize()
+        {
+            var existingToken = await this.localStore.GetItemAsStringAsync("OpenSkyApiToken");
+            var expiration = await this.localStore.GetItemAsync<DateTime?>("OpenSkyApiTokenExpiration");
+            var username = await this.localStore.GetItemAsStringAsync("OpenSkyUser");
+            if (!string.IsNullOrEmpty(existingToken) && expiration.HasValue && expiration.Value > DateTime.Now)
+            {
+                this.IsUserLoggedIn = true;
+                this.OpenSkyApiToken = existingToken;
+                this.Username = username;
+            }
+            else
+            {
+                await this.Logout();
+            }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Informs the service that a user logged in.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 07/05/2021.
+        /// </remarks>
+        /// <param name="token">
+        /// The new OpenSky api token.
+        /// </param>
+        /// <param name="expiration">
+        /// The expiration Date/Time of the token.
+        /// </param>
+        /// <param name="username">
+        /// The username.
+        /// </param>
+        /// <returns>
+        /// An asynchronous result.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public async Task LoggedIn(string token, DateTime expiration, string username)
         {
             if (!string.IsNullOrEmpty(token))
             {
                 this.IsUserLoggedIn = true;
                 this.OpenSkyApiToken = token;
+                this.Username = username;
                 await this.localStore.SetItemAsync("OpenSkyApiToken", token);
+                await this.localStore.SetItemAsync("OpenSkyApiTokenExpiration", expiration);
+                await this.localStore.SetItemAsync("OpenSkyUser", username);
                 if (this.NotifyUserChanged != null)
                 {
-                    await this.NotifyUserChanged.Invoke(token);
+                    await this.NotifyUserChanged.Invoke(true);
                 }
             }
             else
@@ -50,14 +141,28 @@ namespace OpenSky.Website.Services
             }
         }
 
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// User wants to log out.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 07/05/2021.
+        /// </remarks>
+        /// <returns>
+        /// An asynchronous result.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
         public async Task Logout()
         {
             this.IsUserLoggedIn = false;
             this.OpenSkyApiToken = null;
+            this.Username = null;
             await this.localStore.RemoveItemAsync("OpenSkyApiToken");
+            await this.localStore.RemoveItemAsync("OpenSkyApiTokenExpiration");
+            await this.localStore.RemoveItemAsync("OpenSkyUser");
             if (this.NotifyUserChanged != null)
             {
-                await this.NotifyUserChanged.Invoke(null);
+                await this.NotifyUserChanged.Invoke(false);
             }
         }
     }
