6e9072d0688a3c68ded70347ff36a8d7b87e5ffc
sushi.at <sushi@opensky.to>
2023-11-21 12:01:42 +0000

    Removed local docker scripts
    Not useful running release code for testing

--- build_docker.bat
+++
@@ -1,1 +0,0 @@
-docker build -f ".\OpenSky.Website\Dockerfile" --force-rm --no-cache -t openskywebsite --build-arg "BUILD_CONFIGURATION=Release" --label "com.microsoft.created-by=visual-studio" --label "com.microsoft.visual-studio.project-name=OpenSky.Website" .
